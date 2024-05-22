ff50d47cbffa54b250c53230d6db359835caba4f
xaviblast123 <xanaranjo@gmail.com>
2024-05-22 11:58:55 -0700

    This is the stuff done on May 22

--- scripts/X/attempt_1.py
+++ scripts/X/attempt_1.py
@@ -11,15 +11,31 @@ import matplotlib.pyplot as plt
 
 A = 0
 B = 0
-delta = 0.01
-n = 20
+zetta_s = 0.01
+n = 1
+delta = 1/(n+1)
 zetta = n*delta
-second_der = 0
 
-goo = np.exp(2*A)
-grr = np.exp(2*B)
 
-goo_approx = 2*np.exp(A)*np.sinh(A)
+#Finite differences attempt 1
+def finite_differences(A, B):
+    goo = np.exp(2*A) #g_00 guess
+    grr = np.exp(2*B) #g_rr guess
+    goo_approx = 2*np.exp(A)*np.sinh(A) #approx. used for g_00(1-1/g_00)
+    C = -(goo/grr)*(zetta_s**2/((4*zetta**2)*(zetta**2-zetta_s**2)))+(2/zetta_s)*goo_approx+2*(goo/grr)/delta**2 #first value
+    D = -(goo/grr)/delta**2 #second value
+    matrix = [[C, D],[D, C]]
+    lambdan = np.diag(matrix)
+    epsilon = lambdan/(1+np.sqrt(1+zetta_s*lambdan/2))
+    return epsilon 
 
-for value in range(n):
-    second_der += 
+#now, attempting RK
+def Runge_Kutta():
+    A_0 = B_0 = 0
+    A = A_0
+    B = B_0
+    goo = np.exp(2*A)
+    grr = np.exp(2*B)
+    A_temp = A_0 + (1/(2*zetta))*(grr-1)-(zetta_s*zetta/4)*grr
+
+print(finite_differences(A, B))
