a89d1979eebec71fdd16071ddb1bcee2e5a260ed
captainj359 <jjherman359@gmail.com>
2024-05-22 12:12:49 -0700

    matrix was added

--- scripts/find_energies_main.py
+++ scripts/find_energies_main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 A = 0 #initial condition for time metric  
 B = 0 #initial condition for radial matric 
-n = 20 # steps
+n = 2 # steps
 delta = 1 / (n+1) # finite difference?
 zeta = n * delta
 zeta_s = 0.01 #initialized from some eigenvalues
@@ -14,9 +14,23 @@ m_pl = 1 / np.sqrt(G) #mass of plank mass
 m = 8.2*10**10 #if a equals the atomic Bohr radius
 a = 1 / (G*m**3)#gravitational bohr radius
 r_s = 2*G*m #schwarzschild radius
-H = -(zeta_s**2)/(4*(zeta**2)*(zeta**2 - zeta_s**2))  # this is h~"/h~ = H
 
-C = (-(g00/grr)*H + (4/zeta_s)*np.exp(A)*np.sinh(A)+ 2*(np.sqrt(g00/grr)/(delta**2)))
-D = -np.sqrt(g00/grr)/(delta**2)
+H = -(zeta_s**2)/(4*(zeta**2)*(zeta**2 - zeta_s**2))  # this is h_~"/h_~ = H
 
-print(C)
+C = (-(g00/grr)*H + (4/zeta_s)*np.exp(A)*np.sinh(A)+ 2*((g00/grr)/(delta**2)))#matrix component 
+D = -(g00/grr)/(delta**2)#matrix component
+
+def lambda_n(n):
+
+    matrix = np.array([[C,D],[D,C]])
+    e_vec, e_vals = np.linalg.eig(matrix)
+
+    diag_matrix = np.diag(e_vals)
+
+    return diag_matrix
+
+print("matrix", lambda_n(n))
+
+epsilon = lambda_n(n)/(1+np.sqrt(1 + (zeta_s*lambda_n(n))/2))
+
+print("Epsilon",epsilon)
